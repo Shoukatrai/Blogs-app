a2f7208276eb6d7df0a102fb22cb9c4f14e5b894
Shoukatrai <raishoukat033@gmail.com>
2025-01-19 15:12:45 +0500

    commit

--- Dashboard/dash.js
+++ Dashboard/dash.js
@@ -1,75 +1,85 @@
-import { app, auth ,collection,db , doc ,getDocs, setDoc , addDoc } from "../firebase.js";
+import { app, auth, collection, db, doc, getDocs, setDoc, addDoc, deleteDoc } from "../firebase.js";
 
 //getting elements
 const tilteInput = document.querySelector("#tilteInput")
-const BlogText= document.querySelector("#BlogText")
-const checkbox= document.querySelector("#checkbox")
+const BlogText = document.querySelector("#BlogText")
+const checkbox = document.querySelector("#checkbox")
 const Blogs = document.querySelector(".Blogs")
 
 
-const authCheck = ()=>{
+const authCheck = () => {
     const userId = localStorage.getItem("uid")
-    if(!userId){
+    if (!userId) {
         window.location.href = "../login/login.html"
     }
 }
 
 
 
-const showBlogs =async ()=>{
+const showBlogs = async () => {
     try {
         Blogs.innerHTML = "";
         const uid = localStorage.getItem("uid");
-        const snapsot = await getDocs(collection(db , "blogs"))
-        snapsot.forEach((doc)=>{
+        const snapsot = await getDocs(collection(db, "blogs"))
+        snapsot.forEach((doc) => {
             console.log(doc.data().isPrivate)
-            if(doc.data().isPrivate){
-                if(doc.data().uid ===uid){
+            if (doc.data().isPrivate) {
+                if (doc.data().uid === uid) {
                     const UI = `<div class="blog-container">
         <div class="head">
             <h3> ${doc.data().title} </h3>
         <div class="content">
             ${doc.data().text}
+            
         </div>
         <div class="footer">
-          <button>edit</button>
-          <button>delete</button>
+          <button onclick = "editBlog()">edit</button>
+          <button onclick = "deleteBlog()">delete</button>
         </div>
    </div>  `
-    
-    Blogs.innerHTML +=UI
+
+                    Blogs.innerHTML += UI
+
                 }
-            }else{
+            } else {
                 const UI = `<div class="blog-container">
         <div class="head">
             <h3> ${doc.data().title} </h3>
         <div class="content">
             ${doc.data().text}
+            
         </div>
         <div class="footer">
-          <button>edit</button>
-          <button>delete</button>
+          <button id = "editBtn" onclick = "editBlog()">edit</button>
+          <button id = "deletBtn"  onclick = "deleteBlog()">delete</button>
         </div>
    </div> `
-    
-    Blogs.innerHTML +=UI
+
+                Blogs.innerHTML += UI
+                const editBtn = document.getElementById("editBtn")
+                const deletBtn = document.getElementById("deletBtn")
+
+                editBtn.style.pointerEvents = "none"
+                deletBtn.style.pointerEvents = "none"
+                console.log(editBtn)
+                console.log(deletBtn)
             }
-            
-        })  
+
+        })
     } catch (error) {
         alert(error.code)
-    } 
+    }
 }
 
-const blogsHandler =async ()=>{
+const blogsHandler = async () => {
     try {
         const uid = localStorage.getItem("uid")
         console.log(uid)
-        await addDoc(collection(db, "blogs"),{
+        await addDoc(collection(db, "blogs"), {
             title: tilteInput.value,
             text: BlogText.value,
             isPrivate: checkbox.checked,
-            uid:uid
+            uid: uid
         })
         alert("blog created successfully!")
         showBlogs()
@@ -80,19 +90,31 @@ const blogsHandler =async ()=>{
 }
 
 
+const deleteBlog = async () => {
+    const uid = localStorage.getItem("uid")
+    await deleteDoc()
+    console.log(uid)
+    console.log("delete")
+}
 
 
+const editBlog = () => {
+    const uid = localStorage.getItem("uid")
+    console.log(uid)
+    console.log('edit')
+}
 
-
-const logOut = ()=>{
+const logOut = () => {
     let uid = localStorage.getItem("uid")
     uid = ""
-    localStorage.setItem("uid" , uid)
+    localStorage.setItem("uid", uid)
     window.location.replace("../login/login.html")
 }
 
 window.logOut = logOut
 
-window.showBlogs   = showBlogs  
-window.blogsHandler  = blogsHandler 
+window.showBlogs = showBlogs
+window.blogsHandler = blogsHandler
 window.authCheck = authCheck
+window.editBlog = editBlog
+window.deleteBlog = deleteBlog
--- firebase.js
+++ firebase.js
@@ -1,43 +1,44 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-  
-  import { getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-  
-  import { getFirestore , doc ,getDoc,  getDocs , collection, query, where ,setDoc,addDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";  
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 
-  
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDuBvIdsuIzy3q5Ni34A3Hqh5292b5aXf8",
-    authDomain: "blogs-app-8a228.firebaseapp.com",
-    projectId: "blogs-app-8a228",
-    storageBucket: "blogs-app-8a228.firebasestorage.app",
-    messagingSenderId: "854316630713",
-    appId: "1:854316630713:web:62ebd93378b0b7cf1c1168"
-  };
+import { getFirestore, doc, getDoc, getDocs, collection, query, where, setDoc, addDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuBvIdsuIzy3q5Ni34A3Hqh5292b5aXf8",
+  authDomain: "blogs-app-8a228.firebaseapp.com",
+  projectId: "blogs-app-8a228",
+  storageBucket: "blogs-app-8a228.firebasestorage.app",
+  messagingSenderId: "854316630713",
+  appId: "1:854316630713:web:62ebd93378b0b7cf1c1168"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-  
+
 
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-export{
-    app,
-    auth,
-    createUserWithEmailAndPassword,
-    db,
-    doc, setDoc,
-    signInWithEmailAndPassword,
-    getDocs,
-    collection,
-    query, where,
-    getDoc,
-    addDoc
+export {
+  app,
+  auth,
+  createUserWithEmailAndPassword,
+  db,
+  doc, setDoc,
+  signInWithEmailAndPassword,
+  getDocs,
+  collection,
+  query, where,
+  getDoc,
+  addDoc,
+  deleteDoc
 }
